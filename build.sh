87a41241de058cf276bbf848fbe19f8254500d8a
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2025-11-29 03:41:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,22 +39,80 @@ printf '|:---|:---|:---|:---|\n' >> "$TABLE_CONTENT_FILE"
 
 
 printf 'Starting to process userscripts in %s...\n' "$SRC_DIR"
+declare -i userscript_count=$(find "$SRC_DIR" -name "*.user.js" | wc -l)
 declare -i loc_count_total=0
+declare -a ms_ids
+declare -A ms_store
 for file in "$SRC_DIR"/*.user.js; do
   id=$(basename "$file" .user.js)
   printf '>> Processing ID: %s\n' "$id"
 
 
+  if [[ "$id" = 'zzzzzzzz' ]]; then
+    printf '   0. Generating src file for megascript\n'
+    cat > "$file" << EOF
+// ==UserScript==
+// @name         Trakt.tv | Megascript
+// @description  All $((userscript_count - 1)) userscripts from my "Trakt.tv Userscript Collection" repo merged into one for convenience.
+// @version      $(date '+%Y-%m-%d_%H-%M')
+// @namespace    zzzzzzzz
+$(grep -m 1 '// @icon' <<< "${ms_store["${ms_ids[0]}.header"]}")
+$(grep '// @match' <<< "${ms_store["${ms_ids[0]}.header"]}")
+$(grep -m 1 '// @run-at' <<< "${ms_store["${ms_ids[0]}.header"]}")
+$(for id in "${ms_ids[@]}"; do printf '%s\n' "${ms_store["$id.header"]}"; done \
+  | grep -E '^// @(resource|require|grant|connect)' \
+  | sort -u \
+  | awk '
+      /^\/\/ @resource/ {key="a"}
+      /^\/\/ @require/  {key="b"}
+      /^\/\/ @grant/    {key="ca"; if ($0 ~ /GM_/) key="cb"; if ($0 ~ /GM\./) key="cc"}
+      /^\/\/ @connect/  {key="d"}
+      {print key $0}
+    ' \
+  | sort \
+  | sed -E 's|^[a-z]+||')
+// ==/UserScript==
+
+/* README
+### General
+- You can turn off individual modules in the userscript storage tab *(note: only displayed after first run)* by setting their respective id *(see table below)* to false.
+
+| *ID*       | *NAME* |
+| :--------- | :----- |
+$(for id in "${ms_ids[@]}"; do printf '| `%s` | %s |\n' "$id" "$(sed 's#|#\\|#g' <<< "${ms_store["$id.script_name"]}")"; done)
+*/
+$(for id in "${ms_ids[@]}"; do
+  [[ -n "${ms_store["$id.readme_comment"]}" ]] && printf '\n/* README [%s]\n%s\n' "${ms_store["$id.script_name"]}" "$(sed '1d' <<< "${ms_store["$id.readme_comment"]}")"
+done)
+
+'use strict';
+
+const gmStorage = { $(for id in "${ms_ids[@]}"; do printf "['%s']: true, " "$id"; done)...(GM_getValue('megascript')) };
+GM_setValue('megascript', gmStorage);
+$(for id in "${ms_ids[@]}"; do printf "\n\ngmStorage['%s'] && (async () => {\n%s\n})();\n" "$id" "${ms_store["$id.body"]}"; done)
+EOF
+  fi
+
+
   printf '   1. Parsing source file\n'
   clean_content="$(tr -d '\r' < "$file")"
-  script_name="$(grep -m 1 '// @name' <<< "$clean_content" | sed -E 's|// @name\s+||')"
-  script_desc="$(grep -m 1 '// @description' <<< "$clean_content" | sed -E 's|// @description\s+||')"
-  script_version="$(grep -m 1 '// @version' <<< "$clean_content" | sed -E 's|// @version\s+||')"
-  script_namespace="$(grep -m 1 '// @namespace' <<< "$clean_content" | sed -E 's|// @namespace\s+||')"
+  script_name="$(grep -m 1 '^// @name' <<< "$clean_content" | sed -E 's|// @name\s+||')"
+  script_desc="$(grep -m 1 '^// @description' <<< "$clean_content" | sed -E 's|// @description\s+||')"
+  script_version="$(grep -m 1 '^// @version' <<< "$clean_content" | sed -E 's|// @version\s+||')"
+  script_namespace="$(grep -m 1 '^// @namespace' <<< "$clean_content" | sed -E 's|// @namespace\s+||')"
   header="$(sed -n '\|^// ==UserScript==$|,\|^// ==/UserScript==$|p' <<< "$clean_content")"
   readme_comment="$(sed -n '\|^/\* README$|,\|^\*/$|p' <<< "$clean_content")"
   body="$(sed '\|^// ==UserScript==$|,\|^// ==/UserScript==$|d; \|^/\* README$|,\|^\*/$|d' <<< "$clean_content" | sed -n '\|[^\s]|,$p')"
+
   [[ "$id" != "${script_namespace##*/}" ]] && { printf 'Error: Userscript ID from filename does not match ID from namespace. Exiting.\n' >&2; exit 1; }
+
+  if [[ "$id" != 'zzzzzzzz' ]]; then
+    ms_ids+=("$id")
+    ms_store["$id.script_name"]="$script_name"
+    ms_store["$id.header"]="$header"
+    ms_store["$id.readme_comment"]="$readme_comment"
+    ms_store["$id.body"]="$body"
+  fi
 
 
   printf '   2. Generating dist and meta files\n'
@@ -99,7 +157,7 @@ for file in "$SRC_DIR"/*.user.js; do
   loc_count=$(npx cloc --quiet --sum-one --stdin-name="$id.user.js" - <<< "$body" | grep -m 1 'SUM:' | awk '{print $5}')
   loc_count_badge="[![lines of code](https://img.shields.io/badge/loc-$loc_count-orange)](../../$DIST_DIR/$id.user.js)"
   printf '%s\n\n' "$loc_count_badge" >> "$doc_file"
-  ((loc_count_total += loc_count))
+  [[ "$id" != 'zzzzzzzz' ]] && ((loc_count_total += loc_count))
 
   printf '%s' "${readme_comment:+$'## Info\n'"$(sed '1d;$d' <<< "$readme_comment")"$'\n\n'}" >> "$doc_file"
 
@@ -115,7 +173,6 @@ done
 
 
 printf 'Finalizing README.md\n'
-declare -i userscript_count=$(find "$SRC_DIR" -name "*.user.js" | wc -l)
 ( 
   cat "$BEFORE_TABLE_FILE" | sed -E "s|(/badge/loc-)[0-9]+|\1$loc_count_total|; s|(/badge/userscripts-)[0-9]+|\1$userscript_count|"
   head -n 2 "$TABLE_CONTENT_FILE"
