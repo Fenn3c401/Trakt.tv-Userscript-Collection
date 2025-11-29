391bdd33d0cf6317cba037233c03e7974a967306
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2025-11-29 07:23:12 +0100

    Update h8vh5z16.user.js

--- userscripts/src/h8vh5z16.user.js
+++ userscripts/src/h8vh5z16.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Trakt.tv | Director Badge
-// @description  Appends a special "Director" badge to your username because you deserve it. It's usually reserved for team members like Trakt's co-founders Sean and Justin. See https://trakt.tv/users/sean for how it looks.
+// @description  Appends a special "Director" badge to your username because you deserve it. It's usually reserved for team members like Trakt's co-founders Sean and Justin. See https://classic.trakt.tv/users/sean for how it looks.
 // @version      1.0.0
 // @namespace    h8vh5z16
 // @icon         https://trakt.tv/assets/logos/logomark.square.gradient-b644b16c38ff775861b4b1f58c1230f6a097a2466ab33ae00445a505c33fcb91.svg
