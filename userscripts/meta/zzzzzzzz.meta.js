403146f78d11a53086045f11047455d300d85acc
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2025-11-29 07:55:55 +0000

    build: auto-generate meta, dist, docs, megascript and readme

--- userscripts/meta/zzzzzzzz.meta.js
+++ userscripts/meta/zzzzzzzz.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trakt.tv | Megascript
 // @description  All 14 userscripts from my "Trakt.tv Userscript Collection" repo merged into one for convenience. See README for details.
-// @version      2025-11-29_07-24
+// @version      2025-11-29_07-55
 // @namespace    https://github.com/Fenn3c401
 // @author       Fenn3c401
 // @license      GPL-3.0-or-later
--- userscripts/src/zzzzzzzz.user.js
+++ userscripts/src/zzzzzzzz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trakt.tv | Megascript
 // @description  All 14 userscripts from my "Trakt.tv Userscript Collection" repo merged into one for convenience.
-// @version      2025-11-29_07-24
+// @version      2025-11-29_07-55
 // @namespace    zzzzzzzz
 // @icon         https://trakt.tv/assets/logos/logomark.square.gradient-b644b16c38ff775861b4b1f58c1230f6a097a2466ab33ae00445a505c33fcb91.svg
 // @match        https://trakt.tv/*
