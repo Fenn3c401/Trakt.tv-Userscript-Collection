e3e27b19818d1cac4126bb208daa49719727a475
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2025-12-18 04:43:50 +0100

    Update yl9xlca7.user.js

--- userscripts/src/yl9xlca7.user.js
+++ userscripts/src/yl9xlca7.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Trakt.tv | Average Season And Episode Ratings
 // @description  Shows the average general and personal rating of the seasons of a show and the episodes of a season. You can see the averages for all episodes of a show on its /seasons/all page.
-// @version      1.0.3
+// @version      1.0.4
 // @namespace    yl9xlca7
+// @updateURL    https://update.greasyfork.org/scripts/550070.meta.js
 // @icon         https://trakt.tv/assets/logos/logomark.square.gradient-b644b16c38ff775861b4b1f58c1230f6a097a2466ab33ae00445a505c33fcb91.svg
 // @match        https://trakt.tv/*
 // @match        https://classic.trakt.tv/*
