08544173458baba6f0ea78268e98198a788a2ab1
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2026-02-01 03:34:53 +0100

    Update 71cd9s61.user.js

--- userscripts/src/71cd9s61.user.js
+++ userscripts/src/71cd9s61.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trakt.tv | Actor Pronunciation Helper
 // @description  Adds a button on /people pages for fetching an audio recording of that person's name with the correct pronunciation from https://forvo.com
-// @version      1.0.4
+// @version      1.0.6
 // @namespace    71cd9s61
 // @updateURL    https://update.greasyfork.org/scripts/550069.meta.js
 // @icon         https://trakt.tv/assets/logos/logomark.square.gradient-b644b16c38ff775861b4b1f58c1230f6a097a2466ab33ae00445a505c33fcb91.svg
@@ -15,13 +15,15 @@
 // ==/UserScript==
 
 
+/* global moduleName */
+
 'use strict';
 
-let $, toastr;
+let $;
 
 const logger = {
   _defaults: {
-    title: GM_info.script.name.replace('Trakt.tv', 'Userscript'),
+    title: (typeof moduleName !== 'undefined' ? moduleName : GM_info.script.name).replace('Trakt.tv', 'Userscript'),
     toast: true,
     toastrOpt: { positionClass: 'toast-top-right', timeOut: 10000, progressBar: true },
     toastrStyles: '#toast-container#toast-container a { color: #fff !important; border-bottom: dotted 1px #fff; }',
@@ -30,7 +32,7 @@ const logger = {
     const { title = this._defaults.title, toast = this._defaults.toast, toastrOpt, toastrStyles = '', consoleStyles = '', data } = opt,
           fullToastrMsg = `${msg}${data !== undefined ? ' See console for details.' : ''}<style>${this._defaults.toastrStyles + toastrStyles}</style>`;
     console[fnConsole](`%c${title}: ${msg}`, consoleStyles, ...(data !== undefined ? [data] : []));
-    if (toast) toastr[fnToastr](fullToastrMsg, title, { ...this._defaults.toastrOpt, ...toastrOpt });
+    if (toast) unsafeWindow.toastr?.[fnToastr](fullToastrMsg, title, { ...this._defaults.toastrOpt, ...toastrOpt });
   },
   info(msg, opt) { this._print('info', 'info', msg, opt) },
   success(msg, opt) { this._print('info', 'success', msg, { consoleStyles: 'color:#00c853;', ...opt }) },
@@ -45,8 +47,7 @@ document.addEventListener('turbo:load', () => {
   if (!/^\/people\/[^\/]+(\/lists.*)?$/.test(location.pathname)) return;
 
   $ ??= unsafeWindow.jQuery;
-  toastr ??= unsafeWindow.toastr;
-  if (!$ || !toastr) return;
+  if (!$) return;
 
   $(`<button id="btn-pronounce-name">` +
       `<div class="audio-animation fade">` +
