bfae5be7258676db1eb0c72dd05ca55785ee2db7
Fenn3c401 <230925697+Fenn3c401@users.noreply.github.com>
2026-02-13 10:26:23 +0100

    Update x70tru7b.user.js

--- userscripts/src/x70tru7b.user.js
+++ userscripts/src/x70tru7b.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trakt.tv | Partial VIP Unlock
-// @description  Unlocks some vip features: adding titles to maxed-out lists, advanced filters, "more" buttons on dashboard, faster page navigation, bulk list management, rewatching, custom calendars, advanced list progress and more. Also hides some vip advertisements.
-// @version      2.0.5
+// @description  Unlocks some vip features: advanced filters, "more" buttons on dashboard, faster page navigation, bulk list management, rewatching, custom calendars, advanced list progress and more. Also hides some vip advertisements.
+// @version      2.1.0
 // @namespace    x70tru7b
 // @updateURL    https://update.greasyfork.org/scripts/550079.meta.js
 // @icon         https://trakt.tv/assets/logos/logomark.square.gradient-b644b16c38ff775861b4b1f58c1230f6a097a2466ab33ae00445a505c33fcb91.svg
@@ -33,10 +33,6 @@
 - ***watch-now modal country selection***
 
 ### Partial Unlock
-- ***adding an item to maxed-out lists***<br>
-    (See the "List Limits Bypass" section down below, it's kind of like the second example, just automated. So if you've got a list with >= 100 items,
-    you can now directly add a new item to it using the regular ui elems. How long that takes depends on the size of that list, if it's 1000 items you're looking at about 45s until completion..
-    Hefty, but it works. Mind you that this is very much experimental and I can only emphasize the importance of backups here.)
 - ***advanced filters***<br>
     (no saved filters, though you can always just save the url of a search with its specific parameters as a bookmark.. works all the same)
 - ***custom calendars***<br>
@@ -57,6 +53,33 @@ I've got a couple more Trakt.tv userscripts which replicate other vip features i
 
 Though you can always just install the [Trakt.tv \| Megascript](zzzzzzzz.md) instead.
 
+### Semi-Private Notes in Comments
+Trakt supports markdown syntax in comments, including reference-style links which you can misuse as a semi-private notes container like `[//]: # (hidden text goes here)`.
+The raw markdown is of course still accessible to anyone through the Trakt api and the `/comments/<comment-id>.json` endpoint (you yourself can also see the raw version when editing),
+but the content is not rendered in the classic and new web versions, in fact a comment can appear to be completely empty this way. I found this interesting because it's a relatively elegant way to
+work around the max. limit for private notes (currently 100), as the note-comments are still stored directly on your Trakt account on a per-title basis and can easily be accessed on arbitrary
+platforms, including ones that don't support userscripts. It's probably advisable to disguise the note-comments by always adding some generic one-liner.
+
+### Filter-By-Terms Regex
+The filter-by-terms (also called "Filter by Title") function works either server or client-side, depending on whether the exact place you're using it from is paginated or not.
+The `/users/<userslug>/lists`, `/seasons` and `/people` pages are all not paginated, so there the filtering is done client-side, with the input being interpreted as a case-insensitive regular expression.
+All other places where the filter-by-terms function is available are paginated and therefore use server-side filtering, those usually don't allow for regular expressions, with the exception of
+the `/progress` page and list pages. The input is matched against:
+- list title and description for `/users/<userslug>/lists` pages
+- episode title for `/seasons` pages
+- title and character name for `/people` pages
+- episode and show title for `/progress` pages
+- title name for list pages
+*/
+
+/* TODO
+@description Unlocks some vip features: adding titles to maxed-out lists,
+
+- ***adding an item to maxed-out lists***<br>
+    (See the "List Limits Bypass" section down below, it's kind of like the second example, just automated. So if you've got a list with >= 100 items,
+    you can now directly add a new item to it using the regular ui elems. How long that takes depends on the size of that list, if it's 1000 items you're looking at about 45s until completion..
+    Hefty, but it works. Mind you that this is very much experimental and I can only emphasize the importance of backups here.)
+
 ### List Limits Bypass
 Credit for this one goes to [SET19724](https://github.com/SET19724) who pointed out some inconsistencies with the unlocked bulk list actions in an issue.
 Turns out with those it's possible to bypass the imposed limits for both the number of lists and items per list:
@@ -76,23 +99,11 @@ You can grow lists to arbitrary(ish, at ~4100 items I get 400 responses) sizes b
 
 Please don't draw any attention to this. I'd also suggest you make use of the [Trakt.tv \| Scheduled E-Mail Data Exports](2hc6zfyy.md) userscript, just in case.
 
-### Semi-Private Notes in Comments
-Trakt supports markdown syntax in comments, including reference-style links which you can misuse as a semi-private notes container like `[//]: # (hidden text goes here)`.
-The raw markdown is of course still accessible to anyone through the Trakt api and the `/comments/<comment-id>.json` endpoint (you yourself can also see the raw version when editing),
-but the content is not rendered in the classic and new web versions, in fact a comment can appear to be completely empty this way. I found this interesting because it's a relatively elegant way to
-work around the max. limit for private notes (currently 100), as the note-comments are still stored directly on your Trakt account on a per-title basis and can easily be accessed on arbitrary
-platforms, including ones that don't support userscripts. It's probably advisable to disguise the note-comments by always adding some generic one-liner.
 
-### Filter-By-Terms Regex
-The filter-by-terms (also called "Filter by Title") function works either server or client-side, depending on whether the exact place you're using it from is paginated or not.
-The `/users/<userslug>/lists`, `/seasons` and `/people` pages are all not paginated, so there the filtering is done client-side, with the input being interpreted as a case-insensitive regular expression.
-All other places where the filter-by-terms function is available are paginated and therefore use server-side filtering, those usually don't allow for regular expressions, with the exception of
-the `/progress` page and list pages. The input is matched against:
-- list title and description for `/users/<userslug>/lists` pages
-- episode title for `/seasons` pages
-- title and character name for `/people` pages
-- episode and show title for `/progress` pages
-- title name for list pages
+
+restore "new list" creation function via regular button after limit is reached
+allow for bulk list copy to a new list with > 100 items
+allow for bulk list ops. with maxed out lists as target
 */
 
 
@@ -129,15 +140,15 @@ const token = null; // atob(GM_info.script.icon.split(',')[1]).match(/<!-- (.*?)
 
 addStyles();
 
-document.addEventListener('click', (evt) => {
-  const listBtnEl = evt.target.closest('.quick-icons .list, .btn-summary.btn-list, .btn-summary.btn-list .side-btn .icon-add'),
-        popoverEl = evt.target.closest('.popover');
-  if (listBtnEl && !popoverEl) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    addToListBtnOverride.call(listBtnEl);
-  }
-}, { capture: true });
+// document.addEventListener('click', (evt) => {
+//   const listBtnEl = evt.target.closest('.quick-icons .list, .btn-summary.btn-list, .btn-summary.btn-list .side-btn .icon-add'),
+//         popoverEl = evt.target.closest('.popover');
+//   if (listBtnEl && !popoverEl) {
+//     evt.stopPropagation();
+//     evt.preventDefault();
+//     addToListBtnOverride.call(listBtnEl);
+//   }
+// }, { capture: true });
 
 document.addEventListener('turbo:load', async () => {
   $ ??= unsafeWindow.jQuery;
@@ -146,7 +157,7 @@ document.addEventListener('turbo:load', async () => {
   if (!$ || !compressedCache || !Cookies) return;
 
 
-  unsafeWindow.actionList = addToListPopupOverride;
+  // unsafeWindow.actionList = addToListPopupOverride;
   $('body').removeAttr('data-turbo');
   patchUserSettings();
   if (token) $('body:not(.dashboard) .feed-icon.csv').attr('href', location.pathname + '.csv?slurm=' + token + location.search.replace('?', '&'));
@@ -342,11 +353,11 @@ function addStyles() {
 .alert-vip-required {
   display: none !important;
 }
-
-.popover:not(.copy-list) ul.lists li.maxed-out:not(.selected) {
-  text-decoration: line-through dashed 2px;
-}
   `);
+
+// .popover:not(.copy-list) ul.lists li.maxed-out:not(.selected) {
+//   text-decoration: line-through dashed 2px;
+// }
 
   const userslug = document.cookie.match(/(?:^|; )trakt_userslug=([^;]*)/)?.[1];
   if (userslug) {
